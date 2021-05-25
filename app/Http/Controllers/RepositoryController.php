c550c736767897a6b6f50ef80ab102aaa5530ad5
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2021-05-25 07:25:25 -0500

    Ajuste para que el controlador del repositorio este conectado al modelo.

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Respository;
+use App\Models\Repository;
 use Illuminate\Http\Request;
 
 class RepositoryController extends Controller
@@ -41,10 +41,10 @@ class RepositoryController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Respository  $respository
+     * @param  \App\Models\Repository  $repository
      * @return \Illuminate\Http\Response
      */
-    public function show(Respository $respository)
+    public function show(Repository $repository)
     {
         //
     }
@@ -52,10 +52,10 @@ class RepositoryController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Respository  $respository
+     * @param  \App\Models\Repository  $repository
      * @return \Illuminate\Http\Response
      */
-    public function edit(Respository $respository)
+    public function edit(Repository $repository)
     {
         //
     }
@@ -64,10 +64,10 @@ class RepositoryController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Respository  $respository
+     * @param  \App\Models\Repository  $repository
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Respository $respository)
+    public function update(Request $request, Repository $repository)
     {
         //
     }
@@ -75,10 +75,10 @@ class RepositoryController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Respository  $respository
+     * @param  \App\Models\Repository  $repository
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Respository $respository)
+    public function destroy(Repository $repository)
     {
         //
     }
